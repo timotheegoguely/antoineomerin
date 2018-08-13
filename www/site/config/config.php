4e6564e608ef6efcb6118c384215b7a51b2ae165
Timothée Goguely <timotheegoguely@mbp-de-timothee.home>
2018-08-13 17:05:24 +0200

    Basic bilingual setup

--- www/site/config/config.php
+++ www/site/config/config.php
@@ -13,3 +13,23 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+c::set('debug', false);
+
+c::set('languages', array(
+  array(
+    'code'    => 'fr',
+    'name'    => 'French',
+    'default' => true,
+    'locale'  => 'fr_FR',
+    'url'     => '/'
+  ),
+  array(
+    'code'    => 'en',
+    'name'    => 'English',
+    'locale'  => 'en_US',
+    'url'     => '/en'
+  )
+));
+
+//c::set('language.detect', true);
