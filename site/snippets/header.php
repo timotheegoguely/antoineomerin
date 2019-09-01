2a2bcce8e4230fcf6a1ee3f57c8592344c717f83
Timothée Goguely <timotheegoguely@gmail.com>
2019-09-01 15:42:38 +0200

    Add meta tags

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,9 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="generator" content="Kirby (<?= Kirby::version() ?>)">
 
   <title><?= $page->title()->html() ?> · <?= $site->title()->html() ?></title>
 
