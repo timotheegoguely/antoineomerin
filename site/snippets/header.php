c84d008b39b3680a598b369163b2cb77cb10e097
timotheegoguely <timotheegoguely@gmail.com>
2020-10-18 15:14:22 +0200

    Add meta tags, font preload, minified links and update schema data

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,8 +5,18 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="generator" content="Kirby (<?= Kirby::version() ?>)">
-
   <title><?= $page->title()->html() ?> · <?= $site->title()->html() ?></title>
+  <meta name="description" content="photographe vidéaste">
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://antoineomerin.fr/">
+  <meta property="og:title" content="<?= $page->title()->html() ?> · <?= $site->title()->html() ?>">
+  <meta property="og:description" content="photographe vidéaste">
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://antoineomerin.fr/">
+  <meta property="twitter:title" content="<?= $page->title()->html() ?> · <?= $site->title()->html() ?>">
+  <meta property="twitter:description" content="photographe vidéaste">
 
   <script type="text/javascript">
     // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
@@ -14,19 +24,25 @@
     document.documentElement.style.setProperty('--vh', vh+'px');
   </script>
   
+  <link rel="preload" href="assets/fonts/ISOv0.7-Light.woff2" as="font" type="font/woff2" crossorigin>
   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
   <?= css([
-    'assets/css/index.css?'.filemtime('assets/css/index.css'),
+    'assets/css/index.min.css?'.filemtime('assets/css/index.min.css'),
     '@auto'
   ]) ?>  
 
   <script type="application/ld+json">
   {
     "@context": "http://schema.org/",
-    "@type": "Person",
+    "@type": "WebSite",
+    "url": "https://antoineomerin.fr",
     "name": "Antoine Omerin",
-    "description": "photographe vidéaste"
-    "url": "http://antoineomerin.fr",
+    "description": "photographe vidéaste",
+    "creator" : {
+      "@type": "Person",
+      "name": "Antoine Omerin"
+    },
+    "sameAs": "https://www.instagram.com/antoineomerin/"
   }
   </script>
 </head>
