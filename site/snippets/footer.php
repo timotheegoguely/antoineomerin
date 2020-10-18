0162942b48dfc5a631cfdef3cc934f8df2e84e2b
timotheegoguely <timotheegoguely@gmail.com>
2020-10-18 15:15:15 +0200

    Minified index.js

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,6 @@
   <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
   <?= js([
-    'assets/js/index.js',
+    'assets/js/index.min.js',
     '@auto',
   ], true) ?>
   </body>
